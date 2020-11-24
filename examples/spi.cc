d034c9385aedfcaf0aedbe1445de3ae9e3f712a6
Brian Taylor <brian.taylor@bolderflight.com>
2020-11-24 10:50:25 -0700

    Updated example

--- examples/spi.cc
+++ examples/spi.cc
@@ -7,34 +7,24 @@
 
 #include "vector_nav/vector_nav.h"
 
-sensors::VectorNav vn(&SPI, 10);
+sensors::Vn100 vn(&SPI, 10);
 
 int main() {
   Serial.begin(115200);
   while(!Serial) {}
-  vn.Init();
-  sensors::vector_nav::common::SerialNumber sn;
-  sensors::VectorNav::ErrorCode err = vn.ReadRegister(&sn);
-  // if (err == sensors::VectorNav::ERROR_SUCCESS) {
-  //   Serial.println(sn.payload.serial_num);
-  // }
-  // err = vn.WriteSettings();
-  // Serial.println(err);
-  // err = vn.Tare();
-  // Serial.println(err);
-  // err = vn.KnownAccelerationDisturbance(true);
-  // Serial.println(err);
-  // err = vn.KnownMagneticDisturbance(true);
-  // Serial.println(err);
-  // err = vn.KnownAccelerationDisturbance(false);
-  // Serial.println(err);
-  // err = vn.KnownMagneticDisturbance(false);
-  // Serial.println(err);
-  // err = vn.SetGyroBias();
-  // Serial.println(err);
-  // err = vn.SetFilterBias();
-  // Serial.println(err);
-  // vn.Reset();
-  // vn.RestoreFactorySettings();
+  /* Initialize communication */
+  if (!vn.Begin()) {
+    Serial.println(vn.error_code());
+  }
+  while (1) {
+    /* Read sensor and print values */
+    if (vn.Read()) {
+      Serial.print(vn.yaw_rad());
+      Serial.print("\t");
+      Serial.print(vn.pitch_rad());
+      Serial.print("\t");
+      Serial.println(vn.roll_rad());
+    }
+  }
 }
 
